8795951a4dd178abd5c22e29f8209194691491a9
zhangchao <2603144963@qq.com>
2022-07-25 22:02:12 +0800

    第2次推送代码到远程仓库中

--- zhangchaofree/member/my-zhangchaofree-member-biz/src/main/java/org/my/zhangchaofree/member/biz/web/controller/Tacs.java
+++ zhangchaofree/member/my-zhangchaofree-member-biz/src/main/java/org/my/zhangchaofree/member/biz/web/controller/Tacs.java
@@ -3,6 +3,6 @@ package org.my.zhangchaofree.member.biz.web.controller;
 public class Tacs {
 
     public static void main(String[] args) {
-        System.out.println("asd");
+        System.out.println("asdd");
     }
 }
